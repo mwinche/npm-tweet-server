d5afffde446e37181d501fe8363534d095daa466
Matt Winchester <m_winche@yahoo.com>
2015-09-20 17:05:04 -0600

    serving up the page

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 
 var express = require('express');
 var app = express();
+var path = require('path');
+var publicDir = path.join(require('mwinche-tweet-page'), 'public');
+
+app.use('/', express.static(publicDir));
+
+var server = app.listen(3000, function(){
+  console.log('Server started on port 3000');
+});
